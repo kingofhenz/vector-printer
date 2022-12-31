7a535b40ec76dc636025ffd9d0aa41d17925326b
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-12-31 17:52:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,5 +9,10 @@ using namespace std;
 
 int main(){
   vector<int> v1{};
-  
+  int number;
+	int ji = 1;
+	cout << "put your first number" << endl;
+	cin >> number;
+  cout << "put your first number" << endl;\
+  cin >> number;
 }
