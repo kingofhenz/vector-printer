cf1df168657cbf01764336b3c5daf71cf70e1719
kingofhenz <93614808+kingofhenz@users.noreply.github.com>
2022-12-31 18:00:55 -0500

    removed "using namespace std", and also removed extra header files.

--- main.cpp
+++ main.cpp
@@ -2,37 +2,34 @@
 #include <vector>
 #include <string>
 
-using namespace std;
-
 int main() {
-    vector<int> numbers;
-    string input;
+    std::vector<int> numbers;
+    std::string input;
 
     while (true) {
-        cout << "Enter a number or 'end' to stop: ";
-        cin >> input;
+        std::cout << "Enter a number or 'end' to stop: ";
+        std::cin >> input;
 
         if (input == "end") {
             break;
         }
 
         try {
-            int number = stoi(input);
+            int number = std::stoi(input);
             numbers.push_back(number);
-        }
-        catch (const invalid_argument& e) {
-            cout << "Invalid input. Please try again." << endl;
+        } catch (const std::invalid_argument& e) {
+            std::cout << "Invalid input. Please try again." << std::endl;
         }
     }
 
-    cout << "Current vector: [";
+    std::cout << "Current vector: [";
     for (int i = 0; i < numbers.size(); i++) {
-        cout << numbers[i];
+        std::cout << numbers[i];
         if (i < numbers.size() - 1) {
-            cout << ", ";
+            std::cout << ", ";
         }
     }
-    cout << "]" << endl;
+    std::cout << "]" << std::endl;
 
     return 0;
 }
